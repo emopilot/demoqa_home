c4d1709a503331bc32d5bc767b1d87abf2f3b40a
emopilot <alisaazizova99@gmail.com>
2024-11-17 12:33:32 +0300

    gv

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -1,4 +1,3 @@
-import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from pages.base_page import BasePage
@@ -14,22 +13,23 @@ def test_footer_text(browser):
 def test_elements_page_text(browser):
     page = BasePage(browser, base_url='https://demoqa.com/')
     page.visit()
+    print("Visited the homepage.")
 
-    # Нажать на кнопку и ожидать перехода
     elements_button = WebDriverWait(browser, 10).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, '#app > div > div > div.home-body > div > div:nth-child(1)'))
     )
+    print("Elements button found.")
     elements_button.click()
 
-    # Проверить текущий URL
     WebDriverWait(browser, 20).until(
         lambda driver: driver.current_url == 'https://demoqa.com/elements'
     )
+    print(f"Current URL: {browser.current_url}")
     assert browser.current_url == 'https://demoqa.com/elements', "Failed to navigate to /elements"
 
-    # Ожидание заголовка
     center_text = WebDriverWait(browser, 20).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, '.main-header'))
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'div.col-12.mt-4.col-md-6'))
     ).text
+    print(f"Center text: {center_text}")
 
     assert center_text == 'Please select an item from left to start practice.', "Center text does not match"
