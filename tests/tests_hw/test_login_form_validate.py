ea7fb4d9f3a18ee2bc3ee6ae829eb309c41dd49b
emopilot <alisaazizova99@gmail.com>
2024-11-30 14:55:54 +0300

    gv

--- tests/tests_hw/test_login_form_validate.py
+++ tests/tests_hw/test_login_form_validate.py
@@ -1,45 +1,45 @@
-import pytest
-from selenium import webdriver
-from pages.login_form_page import LoginFormPage
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-@pytest.fixture
-def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    yield driver
-    driver.quit()
-
-def test_login_form_validate(driver):
-    expected_placeholders = {
-        "first_name": "First Name",
-        "last_name": "Last Name",
-        "user_email": "name@example.com",
-    }
-    expected_email_pattern = r"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$"
-
-    page = LoginFormPage(driver)
-    page.open()
-
-    first_name_placeholder = page.get_placeholder(page.first_name_input)
-    last_name_placeholder = page.get_placeholder(page.last_name_input)
-    user_email_placeholder = page.get_placeholder(page.email_input)
-    assert first_name_placeholder == expected_placeholders["first_name"], "First Name placeholder incorrect!"
-    assert last_name_placeholder == expected_placeholders["last_name"], "Last Name placeholder incorrect!"
-    assert user_email_placeholder == expected_placeholders["user_email"], "User Email placeholder incorrect!"
-
-    email_pattern = page.get_attribute(page.email_input, "pattern")
-    assert email_pattern == expected_email_pattern, "Email pattern does not match!"
-
-def submit_empty_form(self):
-    submit_button = self.driver.find_element(*self.submit_button)
-
-    WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.submit_button))
-    submit_button.click()
-
-    page.submit_empty_form()
-    assert page.is_form_validated(), "Form is not validated after empty submission!"
-
-
-
+# import pytest
+# from selenium import webdriver
+# from pages.login_form_page import LoginFormPage
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+#
+# @pytest.fixture
+# def driver():
+#     driver = webdriver.Chrome()
+#     driver.maximize_window()
+#     yield driver
+#     driver.quit()
+#
+# def test_login_form_validate(driver):
+#     expected_placeholders = {
+#         "first_name": "First Name",
+#         "last_name": "Last Name",
+#         "user_email": "name@example.com",
+#     }
+#     expected_email_pattern = r"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$"
+#
+#     page = LoginFormPage(driver)
+#     page.open()
+#
+#     first_name_placeholder = page.get_placeholder(page.first_name_input)
+#     last_name_placeholder = page.get_placeholder(page.last_name_input)
+#     user_email_placeholder = page.get_placeholder(page.email_input)
+#     assert first_name_placeholder == expected_placeholders["first_name"], "First Name placeholder incorrect!"
+#     assert last_name_placeholder == expected_placeholders["last_name"], "Last Name placeholder incorrect!"
+#     assert user_email_placeholder == expected_placeholders["user_email"], "User Email placeholder incorrect!"
+#
+#     email_pattern = page.get_attribute(page.email_input, "pattern")
+#     assert email_pattern == expected_email_pattern, "Email pattern does not match!"
+#
+# def submit_empty_form(self):
+#     submit_button = self.driver.find_element(*self.submit_button)
+#
+#     WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.submit_button))
+#     submit_button.click()
+#
+#     page.submit_empty_form()
+#     assert page.is_form_validated(), "Form is not validated after empty submission!"
+#
+#
+#
